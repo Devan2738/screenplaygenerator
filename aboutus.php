319ff506f117cb994af045f0126562a64fe7353b
Devan Karsann <devan28@rocketmail.com>
2018-02-20 00:45:10 -0700

    reformatted header

--- aboutus.php
+++ aboutus.php
@@ -2,7 +2,12 @@
   $pageName = 'about us';
   require_once('header.php');
   ?>
-<p>this is the about us page</p>
+<p>It's better to ride the wave of automation as technology advances than get left behind.</p>
+<p>The media industry generates close to $100 billion a year, and in the cutthroat world of business, companies will definitely
+  pay for SAAS to increase their competitive edge. Software products such as the php powering this website will be developed on the basis of automating the task
+  of media creation. Do you want to make movie scripts on the fly? Create 10 versions of a song at the same time? Incorporate user feeback immediately into tv show productioN? The only limit is your
+  imagination, and here at screenplaygenerator, our imagine is sky high from 8am to 5pm Mon-Fri.</p>
+<p>The softwre solutions you seek are here to find.</p>
 <?php
   require_once('footer.php');
   ?>
