d9811b38cf8e498429c165ec6f113e928718ce79
Devan Karsann <devan28@rocketmail.com>
2018-02-19 20:26:10 -0700

    adding background colors

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
-    <li id="footerLinks" class="horizontalLinks">
-      <a href="contactus.php">contact us</a>
-      <a href="careers.php">careers</a>
-      <a href="commonquestions.php">common questions></a>
-      <a href="aboutus.php">about us</a>
-    </li>
+      <li id="footerLinks" class="horizontalLinks">
+        <a href="contactus.php">contact us</a>
+        <a href="careers.php">careers</a>
+        <a href="commonquestions.php">common questions></a>
+        <a href="aboutus.php">about us</a>
+      </li>
+    </div>
   </div>
 </body>
--- header.php
+++ header.php
@@ -1,9 +1,12 @@
 <body>
-  <div class="">
-  <li id="headerLinks" class="horizontalLinks">
-    <a href="homepage.php">homepage</a>
-    <a href="discover.php">discover screenplays</a>
-    <a href="generate.php">generate screenplays</a>
-    <a href="science.php">show me the science</a>
-    <a href="signinsignup.php">sign in / sign up</a>
-  </li>
+  <div id="firstBackgroundDiv" class="">
+    <div id="secondBackgroundDiv" class="">
+      <li>
+        <a href="homepage.php">homepage</a>
+      </li>
+      <li id="headerLinks" class="horizontalLinks">
+        <a href="discover.php">discover screenplays</a>
+        <a href="generate.php">generate screenplays</a>
+        <a href="science.php">show me the science</a>
+        <a href="signinsignup.php">sign in / sign up</a>
+      </li>
