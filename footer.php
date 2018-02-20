7e12a319eb518cd17c29e600875aadfe9839a234
Devan Karsann <devan28@rocketmail.com>
2018-02-19 21:30:18 -0700

    reformatted header

--- footer.php
+++ footer.php
@@ -1,16 +1,8 @@
         <ul class="horizontalLinks">
-          <li>
-            <a href="contactus.php">contact us</a>
-          </li>
-          <li>
-            <a href="careers.php">careers</a>
-          </li>
-          <li>
-            <a href="commonquestions.php">common questions</a>
-          </li>
-          <li>
-            <a href="aboutus.php">about us</a>
-          </li>
+          <li><a href="contactus.php">contact us</a></li>
+          <li><a href="careers.php">careers</a></li>
+          <li><a href="commonquestions.php">common questions</a></li>
+          <li><a href="aboutus.php">about us</a></li>
         </ul>
       <!--</div>
     </div>-->
--- header.php
+++ header.php
@@ -9,21 +9,11 @@
     <!--<div id="firstBackgroundDiv" class="">
       <div id="secondBackgroundDiv" class="">-->
         <ul class="horizontalLinks">
-          <li>
-          <a href="homepage.php">homepage</a>
-          </li>
+          <li><a href="homepage.php">homepage</a></li>
         </ul>
         <ul class="horizontalLinks">
-          <li>
-            <a href="discover.php">discover screenplays</a>
-          </li>
-          <li>
-            <a href="generate.php">generate screenplays</a>
-          </li>
-          <li>
-            <a href="science.php">show me the science</a>
-          </li>
-          <li>
-            <a href="signinsignup.php">sign in / sign up</a>
-          </li>
+          <li><a href="discover.php">discover screenplays</a></li>
+          <li><a href="generate.php">generate screenplays</a></li>
+          <li><a href="science.php">show me the science</a></li>
+          <li><a href="signinsignup.php">sign in / sign up</a></li>
         </ul>
