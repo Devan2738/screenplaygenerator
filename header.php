06619ce7655ab5f71b37681275dbe36faa49d27e
Devan Karsann <devan28@rocketmail.com>
2018-02-19 21:34:34 -0700

    reformatted header

--- header.php
+++ header.php
@@ -11,9 +11,9 @@
         <ul class="horizontalLinks">
           <li><a href="homepage.php">homepage</a></li>
         </ul>
-        <ul class="horizontalLinks">
-          <li><a href="discover.php">discover screenplays</a></li>
-          <li><a href="generate.php">generate screenplays</a></li>
-          <li><a href="science.php">show me the science</a></li>
-          <li><a href="signinsignup.php">sign in / sign up</a></li>
+        <ul>
+          <li class="horizontalLinks"><a href="discover.php">discover screenplays</a></li>
+          <li class="horizontalLinks"><a href="generate.php">generate screenplays</a></li>
+          <li class="horizontalLinks"><a href="science.php">show me the science</a></li>
+          <li class="horizontalLinks"><a href="signinsignup.php">sign in / sign up</a></li>
         </ul>
