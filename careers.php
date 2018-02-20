f75b88385faa807225877b9c36a78e95bd2dfb5f
Devan Karsann <devan28@rocketmail.com>
2018-02-20 00:12:32 -0700

    reformatted header

--- careers.php
+++ careers.php
@@ -2,8 +2,10 @@
   $pageName = 'careers';
   require_once('header.php');
   ?>
-  <p margin-top: 12px>voluneers welcome!</p>
-  <p margin-bottom: 12px>0 in revenue = 0 paid positions</p>
+  <div id="careerBody">
+    <p>voluneers welcome!</p>
+    <p>0 in revenue = 0 paid positions</p>
+  </div>
 <?php
   require_once('footer.php');
   ?>
