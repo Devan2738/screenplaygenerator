35f608489b1858d360c5f1667dcbbf81502b50bd
Devan Karsann <devan28@rocketmail.com>
2018-02-19 23:38:26 -0700

    reformatted header

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
-        <ul class="horizontalLinks">
-          <li class="footerLinks"><a href="contactus.php">contact us</a></li>
-          <li class="footerLinks"><a href="careers.php">careers</a></li>
-          <li class="footerLinks"><a href="commonquestions.php">common questions</a></li>
-          <li class="footerLinks"><a href="aboutus.php">about us</a></li>
-        </ul>
+        <div id="footerContainer">
+          <ul id="footerLinks" class="horizontalLinks">
+            <li class="footerLinks"><a href="contactus.php">contact us</a></li>
+            <li class="footerLinks"><a href="careers.php">careers</a></li>
+            <li class="footerLinks"><a href="commonquestions.php">common questions</a></li>
+            <li class="footerLinks"><a href="aboutus.php">about us</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   </body>
