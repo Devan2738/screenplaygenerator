52f1b3693c875b8eb4b7fe0480853515ca493d65
Devan Karsann <devan28@rocketmail.com>
2018-03-07 16:42:44 -0700

    working on dao file

--- aboutus.php
+++ aboutus.php
@@ -3,11 +3,11 @@
   require_once('header.php');
   ?>
 <div id="aboutUsPage">
-  <p>It's better to ride the wave of automation as technology advances than get left behind.</p><br>
+  <p>It's better to ride the wave of automation as technology advances than get left behind.</p>
   <p>The media industry generates close to $100 billion a year, and in the cutthroat world of business, companies will definitely
   pay for SAAS to increase their competitive edge. Software products such as the php powering this website will be developed on the basis of automating the task
-  of media creation. Do you want to make movie scripts on the fly? Create 10 versions of a song at the same time? Incorporate user feeback immediately into tv show productioN? The only limit is your
-  imagination, and here at screenplaygenerator, our imagine is sky high from 8am to 5pm Mon-Fri.</p><br>
+  of media creation. Do you want to make movie scripts on the fly? Create 10 versions of a song at the same time? Incorporate user feeback immediately into tv show production? The only limit is your
+  imagination, and here at screenplaygenerator, our imagine is sky high from 8am to 5pm Mon-Fri.</p>
   <p>The softwre solutions you seek are here to find.</p>
 </div>
 <?php
