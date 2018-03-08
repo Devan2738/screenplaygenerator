98f14fc946de92851e652a96937d5e2d19646e79
Devan Karsann <devan28@rocketmail.com>
2018-03-07 17:02:55 -0700

    trying new db name

--- Dao.php
+++ Dao.php
@@ -1,7 +1,7 @@
 <?php
 class Dao {
  private $host = "us-cdbr-iron-east-05.cleardb.net";
- private $db = "heroku_d66a31f2e552f3e"; # private $db = "heroku_f36640796dba974"; 
+ private $db = "heroku_f36640796dba974"; # private $db = "heroku_d66a31f2e552f3e"; 
  private $user = "b007dde3ed6cbd"; #private $user = "b2cf23ed5d39cc";
  private $pass = "def99b02"; #private $pass = "f49471ca";
 
