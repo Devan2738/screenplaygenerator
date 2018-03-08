79ac33fc20119696d3eade299e0828d4bd3813b1
Devan Karsann <devan28@rocketmail.com>
2018-03-07 21:35:57 -0700

    switched back to randomOutput

--- randomOutput1.php
+++ randomOutput1.php
@@ -2,49 +2,47 @@
 
   $pageName = 'testing';
   require_once('header.php');
-  require_once "Dao.php";
-  $dao = new Dao();
-
-  echo "<p>";
-  #for ($x = 0; $x < 10; $x++) {
-    echo "hello good sir";
-    $pronoun = $dao->getPronoun();
-    $verb = $dao->getVerb();
-    $noun = $dao->getNoun();
-    echo strtoupper($pronoun) . " " . $verb . " " . $noun . ".";
-    echo "<br>";
-  #}
-  echo "</p>";
-
-  $dao->close();
-
-  /*$hostname = "us-cdbr-iron-east-05.cleardb.net";
+  #require_once "Dao.php";
+  #$dao = new Dao();
+  $hostname = "us-cdbr-iron-east-05.cleardb.net";
   $db = "heroku_d66a31f2e552f3e";
   $user = "b2cf23ed5d39cc";
   $password = "f49471ca";
-
   $con=mysqli_connect($hostname,$user,$password,$db);
-  // Check connection
   if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-  $sql="SELECT word FROM words LIMIT 100";
-
-  if ($result=mysqli_query($con,$sql)) {
-    echo "<p>";
-    while ($obj=mysqli_fetch_object($result)) {
-      #printf("%s\n",$obj->word);
-      echo $obj->word . " ";
-      echo "<br>";
+  echo "<p>";
+  for ($x = 0; $x < 10; $x++) {
+    echo "(this is part of the loop)";
+    echo "<br>";
+    $sql="SELECT word FROM words WHERE isPronoun = 1 ORDER BY RAND() LIMIT 1";
+    if ($result=mysqli_query($con,$sql)) {
+      while ($obj=mysqli_fetch_object($result)) {
+        $pronoun = $obj->word;
+      }
+      mysqli_free_result($result);
     }
+    $sql="SELECT word FROM words WHERE isVerb = 1 ORDER BY RAND() LIMIT 1";
+    if ($result=mysqli_query($con,$sql)) {
+      while ($obj=mysqli_fetch_object($result)) {
+        $verb = $obj->word;
+      }
+      mysqli_free_result($result);
+    }
+    $sql="SELECT word FROM words WHERE isNoun = 1 ORDER BY RAND() LIMIT 1";
+    if ($result=mysqli_query($con,$sql)) {
+      while ($obj=mysqli_fetch_object($result)) {
+        $noun = $obj->word;
+      }
+      mysqli_free_result($result);
+    }
+    echo "<p>";
+    echo strtoupper($pronoun) . " " . $verb . " " . $noun . ".";
     echo "</p>";
-    // Free result set
-    mysqli_free_result($result);
+    echo "<br>";
   }
+  echo "</p>";
   mysqli_close($con);
-  */
-
-
-
   require_once('footer.php');
 ?>
