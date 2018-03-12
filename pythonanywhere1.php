badbfd0da04a996b106f1e7329feb1885a99af9e
Devan Karsann <devan28@rocketmail.com>
2018-03-12 11:30:04 -0600

    two new tests for python anywhere

--- pythonanywhere1.php
+++ pythonanywhere1.php
@@ -2,10 +2,11 @@
   $pageName = 'python';
   require_once('header.php');
   ?>
+<div>
 <p><b>About Us</b></p><br>
   <p>This is a test for embedding pyton anywhere in the browser.</p>
-  <iframe style="width: 640; height: 480; border: none;" name="embedded_python_anywhere" src="https://www.pythonanywhere.com/embedded3/"></iframe>
 </div>
+<iframe style="width: 640; height: 480; border: none;" name="embedded_python_anywhere" src="https://www.pythonanywhere.com/embedded3/"></iframe>
 <?php
   require_once('footer.php');
   ?>
