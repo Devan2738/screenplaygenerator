aaba51fdd74969ebd160550b6d0b0eada0c66d7e
Devan Karsann <devan28@rocketmail.com>
2018-03-29 16:27:32 -0600

    separating sign in and sign up pages

--- header.php
+++ header.php
@@ -14,8 +14,9 @@
       <div id="onTopOfBackDiv">
         <h1>Screenplay <span><img src="favicon.ico" width=5% text-align: center; alt=""></span> Generator</h1>
           <ul id="linksInTheHeader" margin-left: >
-            <li class="headerLinks"><a href="discover.php">discover</a></li>
+            <!--<li class="headerLinks"><a href="discover.php">discover</a></li>-->
             <li class="headerLinks"><a href="generate.php">generate</a></li>
             <li class="headerLinks"><a href="science.php">science</a></li>
-            <li class="headerLinks"><a href="signinsignup.php">sign in/up</a></li>
+            <li class="headerLinks"><a href="signin.php">sign in</a></li>
+            <li class="headerLinks"><a href="signup.php">sign up</a></li>
           </ul>
---
+++ signin.php
@@ -0,0 +1,31 @@
+<?php
+    $pageName = 'sign in';
+    require_once('header.php');
+?>
+<div id="signInDiv">
+    <form action="signinConfirmation.php">
+        <div class="imgcontainer">
+        </div>
+        <br>
+        <div class="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <button type="submit">Login</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+        </div>
+        <div class="container" style="background-color:#f1f1f1">
+            <button type="button" class="cancelbtn">Cancel</button>
+            <span class="psw">Forgot <a href="#">password?</a></span>
+        </div>
+    </form>
+</div>
+<br>
+<?php
+  require_once('footer.php');
+?>
