1d457999a3d6894ad6971764d4848437f65f176a
Devan Karsann <devan28@rocketmail.com>
2018-03-29 16:54:50 -0600

    correct php echo's

--- signinConfirmation.php
+++ signinConfirmation.php
@@ -4,8 +4,8 @@
   ?>
 <p>You have submitted the sign in form.</p>
 <?php
-<p>echo 'username: ' . htmlspecialchars($_GET["username"]);</p>
-<p>echo 'password: ' . htmlspecialchars($_GET["password"]);</p>
+  echo '<p>username: ' . htmlspecialchars($_GET["username"]) . '</p>';
+  echo '<p>password: ' . htmlspecialchars($_GET["password"]) . '</p>';
   ?>
 <?php
   require_once('footer.php');
--- signupConfirmation.php
+++ signupConfirmation.php
@@ -4,9 +4,9 @@
   ?>
 <p>You have submitted the sign up form.</p>
 <?php
-<p>echo 'email: ' . htmlspecialchars($_GET["email"]);</p>
-<p>echo 'password: ' . htmlspecialchars($_GET["password"]);</p>
-<p>echo 'password-repeat: ' . htmlspecialchars($_GET["password-repeat"]);</p>
+  echo '<p>email: ' . htmlspecialchars($_GET["email"]) . '</p>';
+  echo '<p>password: ' . htmlspecialchars($_GET["password"]) . '</p>';
+  echo '<p>password-repeat: ' . htmlspecialchars($_GET["password-repeat"]) . '</p>';
   ?>
 <?php
   require_once('footer.php');
