745bf8ff0a8d32de08556a4d0c0f455c40da1428
Devan Karsann <devan28@rocketmail.com>
2018-03-29 17:43:35 -0600

    added brackets around function calls

--- randomOutput2.php
+++ randomOutput2.php
@@ -4,18 +4,18 @@
   require_once('header.php');
   #require_once "Dao.php";
   $dao = new Dao();
-
+  echo "<p> here is a basic testing message </p>";
   echo "<p>";
   for ($x = 0; $x < 10; $x++) {
-    $pronoun = $dao->getPronoun;
-    $verb = $dao->getVerb;
-    $noun = $dao->getNoun;
+    $pronoun = $dao->{getPronoun};
+    $verb = $dao->{getVerb};
+    $noun = $dao->{getNoun};
     echo "<p>";
     echo ucfirst($pronoun) . " " . $verb . " the " . $noun . ".";
     echo "</p>";
     echo "<br>";
 }
   echo "</p>";
-  $dao->close;
+  $dao->{close};
   require_once('footer.php');
 ?>
