22e323f2c14c3e983f4668d4f1024f87ea502ac6
Devan Karsann <devan28@rocketmail.com>
2018-03-29 16:13:54 -0600

    updating forms

--- generate.php
+++ generate.php
@@ -12,26 +12,25 @@
       <input type="text" id="main2" name="mainCharacter" placeholder="main character 2..">
 
       <label for="year">Year</label>
-      <select id="length" name="length">
+      <select id="length" name="length" value="2000-present">
         <option value="1900-1950">1900-1950</option>
         <option value="1951-2000">1951-2000</option>
         <option value="2000-present">2000-present</option>
       </select>
 
       <label for="length">Length</label>
-      <select id="length" name="length">
-        <option value="0-30 minutes">short</option>
-        <option value="30-60 minutes">medium</option>
-        <option value="60-90 minutes">not medium or long</option>
-        <option value="90+ minutes">long</option>
+      <select id="length" name="length" value="1">
+        <option value="1">short</option>
+        <option value="2">medium</option>
+        <option value="3">long</option>
+        <option value="4">extra long</option>
       </select>
 
       <label for="genre">Genre</label>
-      <select id="genre" name="genre">
+      <select id="genre" name="genre" value="comedy">
         <option value="comedy">Comedy</option>
         <option value="action">Action</option>
         <option value="drama">Drama</option>
-        <option value="documentary">documentary</option>
       </select>
 
       <input type="submit" value="Submit">
