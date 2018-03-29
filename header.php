a17d7d81b046f6d76f089940d98bf07ae7273cff
Devan Karsann <devan28@rocketmail.com>
2018-03-29 16:05:04 -0600

    starting prioritization of forms

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
       <div id="onTopOfBackDiv">
         <h1>Screenplay <span><img src="favicon.ico" width=5% text-align: center; alt=""></span> Generator</h1>
           <ul id="linksInTheHeader" margin-left: >
-            <li class="headerLinks"><a href="DISCOVER.php">discover</a></li>
+            <li class="headerLinks"><a href="discover.php">discover</a></li>
             <li class="headerLinks"><a href="generate.php">generate</a></li>
             <li class="headerLinks"><a href="science.php">science</a></li>
             <li class="headerLinks"><a href="signinsignup.php">sign in/up</a></li>
