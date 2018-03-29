690d4e0744f496b5c7aec1c82371857b06f2f596
Devan Karsann <devan28@rocketmail.com>
2018-03-29 17:02:10 -0600

    get array fully accessible for sign in and sign up

--- signupConfirmation.php
+++ signupConfirmation.php
@@ -5,8 +5,8 @@
 <p>You have submitted the sign up form.</p>
 <?php
   echo '<p>email: ' . htmlspecialchars($_GET["email"]) . '</p>';
-  echo '<p>password: ' . htmlspecialchars($_GET["password"]) . '</p>';
-  echo '<p>password-repeat: ' . htmlspecialchars($_GET["password-repeat"]) . '</p>';
+  echo '<p>password: ' . htmlspecialchars($_GET["psw"]) . '</p>';
+  echo '<p>password-repeat: ' . htmlspecialchars($_GET["psw-repeat"]) . '</p>';
   ?>
 <?php
   require_once('footer.php');
