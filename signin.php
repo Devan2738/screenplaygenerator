b185d00b167f60698c9616e1e4c4c26b022f1ada
Devan Karsann <devan28@rocketmail.com>
2018-03-29 16:59:23 -0600

    edited sign in confirmation page

--- signin.php
+++ signin.php
@@ -15,9 +15,9 @@
             <input type="password" placeholder="Enter Password" name="psw" required>
 
             <button type="submit">Login</button>
-            <label>
+            <!--<label>
                 <input type="checkbox" checked="checked" name="remember"> Remember me
-            </label>
+            </label>-->
         </div>
         <div class="container" style="background-color:#f1f1f1">
             <button type="button" class="cancelbtn">Cancel</button>
--- signinConfirmation.php
+++ signinConfirmation.php
@@ -4,8 +4,8 @@
   ?>
 <p>You have submitted the sign in form.</p>
 <?php
-  echo '<p>username: ' . htmlspecialchars($_GET["username"]) . '</p>';
-  echo '<p>password: ' . htmlspecialchars($_GET["password"]) . '</p>';
+  echo '<p>username: ' . htmlspecialchars($_GET["uname"]) . '</p>';
+  echo '<p>password: ' . htmlspecialchars($_GET["psw"]) . '</p>';
   ?>
 <?php
   require_once('footer.php');
