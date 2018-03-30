69f0934e2a63b1e55e7f6d59eb7d694d9cf2ceda
Devan Karsann <devan28@rocketmail.com>
2018-03-29 20:10:14 -0600

    added parentheses

--- randomOutput2.php
+++ randomOutput2.php
@@ -2,21 +2,21 @@
 
   $pageName = 'testing';
   require_once('header.php');
-  include("Dao.php");
+  require_once("Dao.php");
   $dao = new Dao();
 
   echo "<p> here is a basic testing message from randomOutpu2.php </p>";
   echo "<p>";
   for ($x = 0; $x < 10; $x++) {
-    $pronoun = $dao->{getPronoun};
-    $verb = $dao->{getVerb};
-    $noun = $dao->{getNoun};
+    $pronoun = $dao->getPronoun();
+    $verb = $dao->getVerb();
+    $noun = $dao->getNoun();
     echo "<p>";
     echo ucfirst($pronoun) . " " . $verb . " the " . $noun . ".";
     echo "</p>";
     echo "<br>";
   }
   echo "</p>";
-  $dao->{close};
+  $dao->close();
   require_once('footer.php');
 ?>
