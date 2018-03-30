23935fb5be65c571c332d7ef0fd691e4004d3ed0
Devan Karsann <devan28@rocketmail.com>
2018-03-29 19:54:45 -0600

    lets see if dao's functions are called now

--- randomOutput2.php
+++ randomOutput2.php
@@ -4,7 +4,7 @@
   require_once('header.php');
   #require_once "Dao.php";
   $dao = new Dao();
-  echo "<p> here is a basic testing message </p>";
+  echo "<p> here is a basic testing message from randomOutpu2.php </p>";
   echo "<p>";
   for ($x = 0; $x < 10; $x++) {
     $pronoun = $dao->{getPronoun};
