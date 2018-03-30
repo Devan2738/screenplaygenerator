6d9b31dbd51f023d8b6b684f944936e7cac66374
Devan Karsann <devan28@rocketmail.com>
2018-03-30 11:21:48 -0600

    db connection is working better now

--- signinConfirmation.php
+++ signinConfirmation.php
@@ -1,67 +1,65 @@
 <?php
   session_start();
   $pageName = 'sign in';
-  /*if (!filter_var($_POST["uname"], FILTER_VALIDATE_EMAIL))  {
+  if (!filter_var($_POST["uname"], FILTER_VALIDATE_EMAIL))  {
     $_SESSION["invalid email address"] = 'true';
     header('Location: ' . 'http://www.screenplaygenerator.com/signin.php');
     exit; // Ensures, that there is no code _after_ the redirect executed
-  }*/
-  require_once('header.php');
+  }
+    require_once('header.php');
 
-      echo '<p> username: ' . htmlspecialchars($_POST["uname"]) . '</p>';
-      echo '<p> password: ' . htmlspecialchars($_POST["psw"]) . '</p>';
+    echo '<p> username: ' . htmlspecialchars($_POST["uname"]) . '</p>';
+    echo '<p> password: ' . htmlspecialchars($_POST["psw"]) . '</p>';
 
-      $hostname = "us-cdbr-iron-east-05.cleardb.net";
-      $db = "heroku_d66a31f2e552f3e";
-      $user = "b2cf23ed5d39cc";
-      $password = "f49471ca";
-      $mysqli = new mysqli($hostname, $user, $password, $db);
-      if($mysqli->connect_error) {
-        exit('Error connecting to database'); //Should be a message a typical user could understand in production
-      }
-      $stmt = $mysqli->prepare("SELECT email, password, signUpDate FROM users WHERE email = ?");
-      $stmt->bind_param("s", $_POST['uname']);
-      $stmt->execute();
-      $stmt->store_result();
-      if($stmt->num_rows === 0) {
-          echo '<p> the username entered was not found in the database </p>';
-          //exit('No rows');
-      }
-      $stmt->bind_result($email, $password, $signUpDate);
-      $stmt->fetch();
-      echo $email; 
-      echo $password;
-      echo $signUpDate;
-      $stmt->close();
-      /*$conn=mysqli_connect($hostname,$user,$password,$db);
-      //if (mysqli_connect_errno()) {
-      //  echo "Failed to connect to MySQL: " . mysqli_connect_error()
-      //}
-      $sql="SELECT * FROM users WHERE email = " . $_POST["uname"];
-      if ($result=mysqli_query($conn,$sql)) {
-        if ($obj=mysqli_fetch_object($result)) {
-          $_SESSION['email address'] = $_POST["uname"];
-          //echo "username was found";
-          $password = $obj->password;
-          echo '$password' . $password;
-          echo '$_POST[psw]' . $_POST["psw"];
-        }
-        mysqli_free_result($result);
-      }*/
-      /*if ($password != $_POST["psw"]){
-        $_SESSION["invalid password"] = 'true';
-        header('Location: ' . 'http://www.screenplaygenerator.com/signin.php');
-        exit; // Ensures, that there is no code _after_ the redirect executed
-      }
-      else {
-        echo 'username and password match an existing account!';
-      }*/
-      if (strcmp($password,$_POST["psw"])){
+    $hostname = "us-cdbr-iron-east-05.cleardb.net";
+    $db = "heroku_d66a31f2e552f3e";
+    $user = "b2cf23ed5d39cc";
+    $dbPassword = "f49471ca";
+    $mysqli = new mysqli($hostname, $user, $dbPassword, $db);
+    if($mysqli->connect_error) {
+      exit('Error connecting to database'); //Should be a message a typical user could understand in production
+    }
+    $stmt = $mysqli->prepare("SELECT email, password, signUpDate FROM users WHERE email = ?");
+    $stmt->bind_param("s", $_POST['uname']);
+    $stmt->execute();
+    $stmt->store_result();
+    if($stmt->num_rows === 0) {
+        echo '<p> the username entered was not found in the database </p>';
+        //exit('No rows');
+    }
+    $stmt->bind_result($email, $password);
+    $stmt->fetch();
+    echo '<p> $email: ' . $email . '</p>';
+    echo '<p> $password: ' . $password . '</p>';
+    $stmt->close();
+    /*$conn=mysqli_connect($hostname,$user,$password,$db);
+    //if (mysqli_connect_errno()) {
+    //  echo "Failed to connect to MySQL: " . mysqli_connect_error()
+    //}
+    $sql="SELECT * FROM users WHERE email = " . $_POST["uname"];
+    if ($result=mysqli_query($conn,$sql)) {
+      if ($obj=mysqli_fetch_object($result)) {
+        $_SESSION['email address'] = $_POST["uname"];
+        //echo "username was found";
+        $password = $obj->password;
         echo '$password' . $password;
         echo '$_POST[psw]' . $_POST["psw"];
-        echo "username and password match an existing account";
       }
-
+      mysqli_free_result($result);
+    }*/
+    /*if ($password != $_POST["psw"]){
+      $_SESSION["invalid password"] = 'true';
+      header('Location: ' . 'http://www.screenplaygenerator.com/signin.php');
+      exit; // Ensures, that there is no code _after_ the redirect executed
+    }
+    else {
+      echo 'username and password match an existing account!';
+    }*/
+    if (strcmp($password,$_POST["psw"])){
+      echo '$password' . $password;
+      echo '$_POST[psw]' . $_POST["psw"];
+      echo "username and password match an existing account";
+    }
 
   require_once('footer.php');
   ?>
