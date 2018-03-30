b5dc981e1bd64f8310c0def24465decde791511b
Devan Karsann <devan28@rocketmail.com>
2018-03-29 22:10:12 -0600

    removed extra paragraph

--- signinConfirmation.php
+++ signinConfirmation.php
@@ -8,7 +8,6 @@
   }
   require_once('header.php');
 
-  <p>You have submitted the sign in form.</p>
   //echo '<p>username: ' . htmlspecialchars($_POST["uname"]) . '</p>';
   //echo '<p>password: ' . htmlspecialchars($_POST["psw"]) . '</p>';
 
