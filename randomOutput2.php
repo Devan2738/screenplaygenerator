b86e8efe7353940b86668bded17441e907ab9a0a
Devan Karsann <devan28@rocketmail.com>
2018-03-29 20:16:46 -0600

    even more basic

--- randomOutput2.php
+++ randomOutput2.php
@@ -6,8 +6,8 @@
   $dao = new Dao();
 
   echo "<p> here is a basic testing message from randomOutpu2.php </p>";
-  echo "<p>";
-  /*for ($x = 0; $x < 10; $x++) {
+  /*echo "<p>";
+  for ($x = 0; $x < 10; $x++) {
     $pronoun = $dao->getPronoun();
     $verb = $dao->getVerb();
     $noun = $dao->getNoun();
@@ -15,8 +15,8 @@
     echo ucfirst($pronoun) . " " . $verb . " the " . $noun . ".";
     echo "</p>";
     echo "<br>";
-  }*/
+  }
   echo "</p>";
-  $dao->close();
+  $dao->close();*/
   require_once('footer.php');
 ?>
