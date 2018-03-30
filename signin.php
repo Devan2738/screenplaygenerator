0c4683fc87533693f46070af555d5819c25bf1c1
Devan Karsann <devan28@rocketmail.com>
2018-03-30 12:53:44 -0600

    added echo

--- signin.php
+++ signin.php
@@ -33,7 +33,7 @@
         <br>
         <div class="container">
             <label for="username"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" value = "<?php $email ?>" name="uname" required>
+            <input type="text" placeholder="Enter Username" value = "<?php echo $email ?>" name="uname" required>
 
             <label for="password"><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="psw" required>
