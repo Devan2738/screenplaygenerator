c34c04f8e6f6a06e857e68b212027e9e593faa6c
Devan Karsann <devan28@rocketmail.com>
2018-03-30 12:50:04 -0600

    added quote marks around email value in form

--- signin.php
+++ signin.php
@@ -33,7 +33,7 @@
         <br>
         <div class="container">
             <label for="username"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" value = <?php $email ?> required>
+            <input type="text" placeholder="Enter Username" value = "<?php $email ?>" name="uname" required>
 
             <label for="password"><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="psw" required>
