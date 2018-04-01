62383b9ee44c94a2f8fe9fc1f6a1f508385379b7
Devan Karsann <devan28@rocketmail.com>
2018-04-01 00:15:45 -0600

    added test messages for contact submission page

--- contactSubmissionConfirmation.php
+++ contactSubmissionConfirmation.php
@@ -3,18 +3,21 @@
   require_once('header.php');
   ?>
 <div>
-
   <?php
     // the message
     $msg = $_POST['subject'];
-    $msg .= "<br>(sent from user '" . $_POST['username'] . "'')";
+    $msg .= "(sent from user '" . $_POST['username'] . "'')";
     // use wordwrap() if lines are longer than 70 characters
     $msg = wordwrap($msg,70);
     // send email
+    // start of testing
+    echo "<p>\$_POST['firstname']: " . $_POST['firstname'] . "</p>";
+    echo "<p>\$_POST['lastname']: " . $_POST['lastname'] . "</p>";
+    echo "<p>\$_POST['Country']: " . $_POST['Country'] . "</p>";
+    echo "<p>\$msg: " . $msg . "</p>";
+    // end of testing
     mail("devankarsann@u.boisestate.edu","contact us form submission from \"" . $_POST['firstname'] . " " . $_POST['lastname'] . "\" in " . $_POST['Country'],$msg);
   ?>
-
-  <p>Thanks for submitting the contact form! We will take your input into consideration and reply if necessary.</p>
 </div>
 <?php
   require_once('footer.php');
