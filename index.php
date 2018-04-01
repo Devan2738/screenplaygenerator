3d1f87292c9a84d86b0588764d29199112730a04
Devan Karsann <devan28@rocketmail.com>
2018-03-31 19:44:08 -0600

    customized homepage message more

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
   {
     echo "<p>To generate screen plays, please sign into your account or make a new account.</p>";
   }
+  else
+  {
+    echo "<p> Are you ready to generate more screenplays? Click on the 'generate' tab when you're ready.</p>";
+  }
   ?>
 <?php
   require_once('footer.php');
