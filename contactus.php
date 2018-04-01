911ed9ff573599b1975a54dee43875256a84af5f
Devan Karsann <devan28@rocketmail.com>
2018-04-01 00:45:43 -0600

    blocking out contact form

--- contactus.php
+++ contactus.php
@@ -2,6 +2,7 @@
   $pageName = 'contact us';
   require_once('header.php');
   ?>
+  <!--
   <div id="contactFormDiv" class="">
     <div class="container">
       <form action="contactSubmissionConfirmation.php" method="post">
@@ -12,14 +13,17 @@
         <label for="lname"><br>Last Name</label>
         <input type="text" id="lname" name="lastname" placeholder="Your last name..">
 
-        <!--<label for="subject"><br>Subject</label>-->
         <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" required></textarea>
-        <input type="hidden" name="username" value="<?php echo $_SESSION['username'] ?>">
+        <input type="hidden" name="username" value=">?php echo $_SESSION['username'] ?>">
         <input type="submit" value="Submit">
-
     </form>
   </div>
 </div>
+-->
+<p>heroko doesn't natively support emailing from php</p>
+<p>we will be working on an add-on or work around soon!</p>
+<br>
+<p>for now, keep your opinions to yourself</p>
 <?php
   require_once('footer.php');
   ?>
