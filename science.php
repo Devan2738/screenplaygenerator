1cd41c448a81730659f6b78c308f2c1623400bc1
Devan Karsann <devan28@rocketmail.com>
2018-04-16 18:33:16 -0600

    escaped username for session

--- science.php
+++ science.php
@@ -3,7 +3,7 @@
   require_once('header.php');
   ?>
   <div id="scienceDiv">
-    <p><b>Science Page</b></p><br>
+    <p><b>You are on the Science Page</b></p><br>
     <p>We will provide a brief explanation of how our website generates screenplays.</p><br>
     <p>Sentences are generally made up of independent and depenedent clauses. A simple tree structure can determine, from a higher level, what clauses a random sentence could consist of.
     Then, each clause can be filled in with the required parts of speech from a database. Setting up coherant adjadenct sentences introduces more complexities, and is perhaps outside
