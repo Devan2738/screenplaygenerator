752e1c10298ffd3140d9cb830295a11fbcbe443a
Devan Karsann <devan2738@gmail.com>
2018-05-17 13:41:53 -0600

    timer is inline now

--- countup.php
+++ countup.php
@@ -1,11 +1,11 @@
 <?php
-  $pageName = 'detect';
+  $pageName = 'countup';
   require_once('header.php');
   ?>
   <script>
     window.onload=function() {
       // Month,Day,Year,Hour,Minute,Second
-      upTime('jan,01,2013,00:00:00'); // ****** Change this line!
+      upTime('jan,01,1988,00:00:00'); // ****** Change this line!
     };
     function upTime(countTo) {
       now = new Date();
@@ -27,18 +27,19 @@
       upTime.to=setTimeout(function(){ upTime(countTo); },1000);
     }
   </script>
-  <div id="scienceDiv">count up page.</p><br>
+  <div id="scienceDiv">Murie design group has been in business for: </p><br>
     <div id="countup">
-      <p id="years">00</p>
-      <p class="timeRefYears">years</p>
-      <p id="days">00</p>
-      <p class="timeRefDays">days</p>
-      <p id="hours">00</p>
-      <p class="timeRefHours">hours</p>
-      <p id="minutes">00</p>
-      <p class="timeRefMinutes">minutes</p>
-      <p id="seconds">00</p>
-      <p class="timeRefSeconds">second</p>
+      <span id="years">00</span>
+      <span class="timeRefYears"> years </span>
+      <span id="days">00</span>
+      <span class="timeRefDays"> days </span>
+      <span id="hours">00</span>
+      <span class="timeRefHours"> hours </span>
+      <span id="minutes">00</span>
+      <span class="timeRefMinutes"> minutes </span>
+      <span id="seconds">00</span>
+      <span class="timeRefSeconds"> seconds </span>
+      <!-- java code for counting up found at https://trulycode.com/bytes/count-date-time-javascript/ -->
     </div>
   </div>
 <?php
